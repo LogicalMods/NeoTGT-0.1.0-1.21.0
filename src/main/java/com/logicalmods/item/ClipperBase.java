1313b5e8c9ef86b72015f48ef46ddf944932bbcb
Logical Modifications <michael.regina@proton.me>
2024-08-15 17:20:09 -0400

    initial commit, rebuilt on NeoForge

--- src/main/java/com/logicalmods/item/ClipperBase.java
+++ src/main/java/com/logicalmods/item/ClipperBase.java
@@ -1,8 +1,6 @@
 package com.logicalmods.item;
 
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.ShearsItem;
-import net.minecraft.world.item.Tier;
 
 public class ClipperBase extends ShearsItem {
 
--- src/main/java/com/logicalmods/item/SplitterBase.java
+++ src/main/java/com/logicalmods/item/SplitterBase.java
@@ -1,13 +1,8 @@
 package com.logicalmods.item;
 
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.item.AxeItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Tier;
-import net.minecraft.world.item.TieredItem;
-import net.neoforged.neoforge.common.SimpleTier;
-
-import java.util.ArrayList;
 
 public class SplitterBase extends AxeItem {
 
